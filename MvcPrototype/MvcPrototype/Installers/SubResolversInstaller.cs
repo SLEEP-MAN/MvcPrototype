f4b1f45af3d3ecf126fe6b3760711f9ac2f2d123
Maciej Nowicki <maciej.j.nowicki@gmail.com>
2015-12-02 20:27:21 +0100

    logger as dependency?

---
+++ MvcPrototype/MvcPrototype/Installers/SubResolversInstaller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using MvcPrototype.SubResolvers;
+
+namespace MvcPrototype.Installers
+{
+    public class SubResolversInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Kernel.Resolver.AddSubResolver(new LoggerSubDependencyResolver());
+        }
+    }
+}
